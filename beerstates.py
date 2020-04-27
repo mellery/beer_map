a9cfd4642f20fc02a5886625ca2a70bf2ab791f0
Mike Ellery <mellery@gmail.com>
2020-04-27 11:04:59 -0400

    add file

---
+++ beerstates.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1], projection=ccrs.LambertConformal())
+
+ax.set_extent([-125, -66.5, 20, 50], ccrs.Geodetic())
+
+shapename = 'admin_1_states_provinces_lakes_shp'
+states_shp = shpreader.natural_earth(resolution='110m',
+                                     category='cultural', name=shapename)
+
+ax.background_patch.set_visible(False)
+ax.outline_patch.set_visible(False)
+
+ax.set_title('Beers from Each State')
+
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+nothad = ["AL", "AK", "AZ", "AR", 
+          "HI", "ID", "KS",    
+          "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+          "NM", "ND", "RI", 
+          "SD", "WY"]
+
+for astate in shpreader.Reader(states_shp).records():
+
+    edgecolor = 'black'
+
+    if astate.attributes['postal'] in nothad:
+        facecolor = "red"
+    else:
+        facecolor = "green"
+
+    ax.add_geometries([astate.geometry], ccrs.PlateCarree(),
+                      facecolor=facecolor, edgecolor=edgecolor)
+
+plt.show()
